ef7b8c8bf9592fb5ea2b617f4f73a06168f23b22
STU6266 <skemendics@byupathway.edu>
2026-01-05 10:46:09 +0100

    new file:   .gitignore 	deleted:    backend/.gitignore 	deleted:    backend/.vscode/launch.json 	deleted:    backend/app.js 	deleted:    backend/contollers/professional.js 	deleted:    backend/db/connect.js 	deleted:    backend/routes.rest 	deleted:    backend/routes/professional.js 	deleted:    backend/server.js 	deleted:    backend/user.json 	new file:   controllers/users.js 	new file:   data/database.js 	deleted:    frontend/index.html 	deleted:    frontend/script.js 	deleted:    frontend/style.css 	modified:   node_modules/.package-lock.json 	renamed:    backend/package-lock.json -> package-lock.json 	renamed:    backend/package.json -> package.json 	new file:   routes/index.js 	new file:   routes/users.js 	new file:   server.js

---
+++ routes/users.js
@@ -0,0 +1,9 @@
+// routes/users.js
+const express = require('express');
+const router = express.Router();
+
+const usersController = require('../controllers/users');
+
+router.get('/', usersController.getAll);
+router.get('/:id', usersController.getSingle);
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,26 @@
+// server.js
+const express = require('express');
+const cors = require('cors');
+
+const routes = require('./routes');
+
+const mongodb = require('./data/database');
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+app.use('/', require('./routes'));
+
+
+
+mongodb.initDb((err) => {
+  if(err) {
+    console.log(err);
+  }
+  else {
+    app.listen(port, () => {console.log(`Database is listening and node Running on port ${port}`)});
+  }
+});
+
+
+
